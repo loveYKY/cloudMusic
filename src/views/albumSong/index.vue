47998c61ab1332e4485af962a76a15185eba52d5
loveYKY <1340520415@qq.com>
2022-07-22 11:45:17 +0800

    修改文件名

---
+++ src/views/albumSong/index.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
